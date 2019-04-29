ee466e04500d581678edf88e83186cbe3faf5016
Dubwin <dubwin@yandex.ru>
2019-04-30 00:30:01 +0300

    take_cart

--- interface.rb
+++ interface.rb
@@ -2,8 +2,19 @@ require_relative "deck"
 require_relative "user"
 
 deck = Deck.new
+
 puts deck.deck
 puts "Как Вас зовут?"
 name = gets.chomp
-user = User.new(name, 100, deck.deck.last)
+user = User.new(name, 100, take_cart(2))
 puts user.hand
+puts deck.deck
+# нужен метод с аргументом, возвращающий карты
+
+def take_cart(n)
+  n.times do |value|
+    current_hand = []
+    current_hand << deck.deck.last
+    deck.deck.pop!
+  end
+end
