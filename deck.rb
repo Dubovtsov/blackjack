c16b460146b809d83e59e6a2fdd950e44c74891e
Dubwin <dubwin@yandex.ru>
2019-04-29 23:38:17 +0300

    deck

--- deck.rb
+++ deck.rb
@@ -4,8 +4,11 @@ class Deck
   include DeckHelper
 
   def initialize
-    
+    deck = mix_deck
   end
 
   # метод рандомайзер
+  def mix_deck
+    deck = Spades.concat(Hearts, Clubs, Diamonds).shuffle
+  end
 end
--- deck_helper.rb
+++ deck_helper.rb
@@ -1,76 +1,77 @@
+# Helper for Deck
 module DeckHelper
-  module ClassMethods
-    # пики
-    Spades = [
-      {name: '2♠', number: 2},
-      {name: '3♠', number: 3},
-      {name: '4♠', number: 4},
-      {name: '5♠', number: 5},
-      {name: '6♠', number: 6},
-      {name: '7♠', number: 7},
-      {name: '8♠', number: 8},
-      {name: '9♠', number: 9},
-      {name: '10♠', number: 10},
-      {name: 'J♠', number: 10},
-      {name: 'Q♠', number: 10},
-      {name: 'K♠', number: 10},
-      {name: 'T♠', number: 10}
-    ]
+  # пики
+  Spades = [
+    {name: '2♠', number: 2},
+    {name: '3♠', number: 3},
+    {name: '4♠', number: 4},
+    {name: '5♠', number: 5},
+    {name: '6♠', number: 6},
+    {name: '7♠', number: 7},
+    {name: '8♠', number: 8},
+    {name: '9♠', number: 9},
+    {name: '10♠', number: 10},
+    {name: 'J♠', number: 10},
+    {name: 'Q♠', number: 10},
+    {name: 'K♠', number: 10},
+    {name: 'T♠', number: 10}
+  ]
 
-    # червы
-    Hearts = [
-      {name: '2♥', number: 2},
-      {name: '3♥', number: 3},
-      {name: '4♥', number: 4},
-      {name: '5♥', number: 5},
-      {name: '6♥', number: 6},
-      {name: '7♥', number: 7},
-      {name: '8♥', number: 8},
-      {name: '9♥', number: 9},
-      {name: '10♥', number: 10},
-      {name: 'J♥', number: 10},
-      {name: 'Q♥', number: 10},
-      {name: 'K♥', number: 10},
-      {name: 'T♥', number: 10}
-    ]
+  # червы
+  Hearts = [
+    {name: '2♥', number: 2},
+    {name: '3♥', number: 3},
+    {name: '4♥', number: 4},
+    {name: '5♥', number: 5},
+    {name: '6♥', number: 6},
+    {name: '7♥', number: 7},
+    {name: '8♥', number: 8},
+    {name: '9♥', number: 9},
+    {name: '10♥', number: 10},
+    {name: 'J♥', number: 10},
+    {name: 'Q♥', number: 10},
+    {name: 'K♥', number: 10},
+    {name: 'T♥', number: 10}
+  ]
 
-    # бубны
-    Diamonds = [
-      {name: '2♦', number: 2},
-      {name: '3♦', number: 3},
-      {name: '4♦', number: 4},
-      {name: '5♦', number: 5},
-      {name: '6♦', number: 6},
-      {name: '7♦', number: 7},
-      {name: '8♦', number: 8},
-      {name: '9♦', number: 9},
-      {name: '10♦', number: 10},
-      {name: 'J♦', number: 10},
-      {name: 'Q♦', number: 10},
-      {name: 'K♦', number: 10},
-      {name: 'T♦', number: 10}
-    ]
+  # бубны
+  Diamonds = [
+    {name: '2♦', number: 2},
+    {name: '3♦', number: 3},
+    {name: '4♦', number: 4},
+    {name: '5♦', number: 5},
+    {name: '6♦', number: 6},
+    {name: '7♦', number: 7},
+    {name: '8♦', number: 8},
+    {name: '9♦', number: 9},
+    {name: '10♦', number: 10},
+    {name: 'J♦', number: 10},
+    {name: 'Q♦', number: 10},
+    {name: 'K♦', number: 10},
+    {name: 'T♦', number: 10}
+  ]
     
-    # трефы
-    Clubs = [
-      {name: '2♣', number: 2},
-      {name: '3♣', number: 3},
-      {name: '4♣', number: 4},
-      {name: '5♣', number: 5},
-      {name: '6♣', number: 6},
-      {name: '7♣', number: 7},
-      {name: '8♣', number: 8},
-      {name: '9♣', number: 9},
-      {name: '10♣', number: 10},
-      {name: 'J♣', number: 10},
-      {name: 'Q♣', number: 10},
-      {name: 'K♣', number: 10},
-      {name: 'T♣', number: 10}
-    ]
+  # трефы
+  Clubs = [
+    {name: '2♣', number: 2},
+    {name: '3♣', number: 3},
+    {name: '4♣', number: 4},
+    {name: '5♣', number: 5},
+    {name: '6♣', number: 6},
+    {name: '7♣', number: 7},
+    {name: '8♣', number: 8},
+    {name: '9♣', number: 9},
+    {name: '10♣', number: 10},
+    {name: 'J♣', number: 10},
+    {name: 'Q♣', number: 10},
+    {name: 'K♣', number: 10},
+    {name: 'T♣', number: 10}
+  ]
+
+  module ClassMethods
   end
   
   module InstanceMethods
-    
   end
   
   def self.included(base)
