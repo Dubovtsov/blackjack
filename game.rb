bfa92ded0a27fdfb3a80dde0906793ff9283c33c
Dubovtsov <dubwin@yandex.ru>
2019-05-20 05:48:14 -0700

    2019-05-20

--- game.rb
+++ game.rb
@@ -20,7 +20,104 @@ class Game
   end
 
   def run
-    @interface.start_game(@current_deck, @bank, @dealer)
-    @interface.game
+    initial_conditions (@interface.start_game)
+    @interface.main_info(@user, @dealer, @bank)
+    @interface.show_menu
+    @interface.game_cards_hands(@user, @dealer)
+    loop do
+      choise = gets.chomp
+      case choise
+      when '1'
+        move
+      when '2'
+        skip
+      when '3'
+        @interface.show(@user, @dealer, @bank)
+      when '4'
+        again
+        break if @quit == 'break'
+      when '5'
+        Game.new.run
+      when '6'
+        @interface.message('Будем рады видеть Вас снова!')
+        break
+      else
+        @interface.message('Неверный ввод!')
+      end
+      rescue StandardError => e
+        puts e.message
+        puts e.backtrace
+    end
+  end
+
+  def initial_conditions(name)
+    @user = User.new(name, 100)
+    user_move(2)
+    @user.bet(@bank, 10)
+    dealer_move(2)
+    @dealer.bet(@bank, 10)
+  end
+
+  def move
+    if !@user.hand.losing
+      user_move(1)
+      @interface.cards_in_hand(@user, 'show')
+      if @dealer.hand.scoring < 17
+        dealer_move(1)
+        @interface.cards_in_hand(@dealer)
+      else
+        @interface.message_skip
+        @interface.cards_in_hand(@dealer)
+      end
+    else
+      @interface.message('Достаточно!')
+      if @dealer.hand.scoring < 17
+        dealer_move(1)
+        @interface.game_card_hands
+      else
+        @interface.message_skip
+        cards_in_hand(@dealer)
+      end
+    end
+    @interface.show_menu
+  end
+
+  def skip
+    if @dealer.hand.scoring < 17
+      dealer_move(1)
+      @interface.game_cards_hands(@user, @dealer)
+    else
+      @interface.message_skip
+      @interface.game_cards_hands(@user, @dealer)
+    end
+    @interface.show_menu
+  end
+
+  def dealer_move(num_of_cards)
+    @interface.message_dealer_move
+    @interface.loading
+    @dealer.take_card(num_of_cards, @current_deck)
+  end
+
+  def user_move(num_of_cards)
+    @user.take_card(num_of_cards, @current_deck)
+  end
+
+  def again
+    if @user.cash >= 10 && @dealer.cash >= 10
+      @current_deck = Deck.new
+      @user.hand = Hand.new
+      user_move(2)
+      @user.bet(@bank, 10)
+      @dealer.hand = Hand.new
+      dealer_move(2)
+      @dealer.bet(@bank, 10)
+      @interface.main_info(@user, @dealer, @bank)
+      @interface.game_cards_hands(@user, @dealer)
+      @interface.show_menu
+    else
+      @interface.message('Недостаточно средств для ставки. Игра окончена!')
+      @quit = 'break'
+    end
   end
 end
--- interface.rb
+++ interface.rb
@@ -21,38 +21,6 @@ class Interface
     @menu = MENU
   end
 
-  def game
-    loop do
-      choise = gets.chomp
-      case choise
-      when '1'
-        system_clear
-        move
-      when '2'
-        system_clear
-        skip
-      when '3'
-        system_clear
-        show
-      when '4'
-        system_clear
-        again
-        break if @quit == 'break'
-      when '5'
-        system_clear
-        Game.new.run
-      when '6'
-        message('Будем рады видеть Вас снова!')
-        break
-      else
-        message('Неверный ввод!')
-      end
-      rescue StandardError => e
-        puts e.message
-        puts e.backtrace
-    end
-  end
-
   def loading
     3.times do
       sleep 0.5
@@ -111,56 +79,28 @@ class Interface
     end
   end
 
-  def skip
-    message_dealer_move
-    loading
-    if @dealer.hand.scoring < 17
-      dealer_move(1)
-      game_card_hands
-    else
-      separator
-      message_skip
-      game_card_hands
-    end
-    show_menu
+  def game_return(user, dealer, bank)
+    bank.return_bet(user)
+    bank.return_bet(dealer)
+    main_info(user, dealer, bank)
   end
 
-  def game_return
-    @bank.return_bet(@user)
-    @bank.return_bet(@dealer)
-    main_info
-  end
-
-  def game_output_info
-    message_cards(@dealer)
+  def game_output_info(user, dealer)
+    message_cards(dealer)
     separator
-    withseparator cards_in_hand(@dealer, 'show')
+    withseparator cards_in_hand(dealer, 'show')
     print "\n"
 
-    message_cards(@user)
+    message_cards(user)
     separator
-    withseparator cards_in_hand(@user, 'show')
+    withseparator cards_in_hand(user, 'show')
     print "\n"
   end
 
-  def start_game(deck, bank, dealer)
-    @current_deck = deck
-    @bank = bank
-    @dealer = dealer
+  def start_game
     name = user_input('Как Вас зовут?')
     message("Добро пожаловать в игру, #{name}!")
-    initial_conditions(name)
-  end
-
-  def initial_conditions(name)
-    @user = User.new(name, 100)
-    user_move(2)
-    @user.bet(@bank, 10)
-    dealer_move(2)
-    @dealer.bet(@bank, 10)
-    main_info
-    game_card_hands
-    withseparator show_menu
+    name
   end
 
   def cards_in_hand(player, show = nil)
@@ -175,96 +115,41 @@ class Interface
   end
 
 
-  def game_card_hands
+  def game_cards_hands(user, dealer)
     puts 'У Вас в руке:'
     separator
-    withseparator cards_in_hand(@user, 'show')
+    withseparator cards_in_hand(user, 'show')
     puts 'В руке Дилера:'
     separator
-    withseparator cards_in_hand(@dealer)
+    withseparator cards_in_hand(dealer)
   end
 
-  def move
-    if !@user.hand.losing
-      withseparator user_move(1)
-      withseparator cards_in_hand(@user, 'show')
-      message_dealer_move
-      withseparator loading
-      if @dealer.hand.scoring < 17
-        dealer_move(1)
-        withseparator cards_in_hand(@dealer)
-      else
-        message_skip
-        withseparator cards_in_hand(@dealer)
-      end
-    else
-      message('Достаточно!')
-      message_dealer_move
-      loading
-      if @dealer.hand.scoring < 17
-        dealer_move(1)
-        game_card_hands
-      else
-        message_skip
-        withseparator cards_in_hand(@dealer)
-      end
-    end
-    show_menu
+  def main_info(user, dealer, bank)
+    show_account('На вашем счёте:', user.cash)
+    show_account('Счёт Дилера:', dealer.cash)
+    message_bank(bank.bank_amount)
   end
 
-  def main_info
-    show_account('На вашем счёте:', @user.cash)
-    show_account('Счёт Дилера:', @dealer.cash)
-    message_bank(@bank.bank_amount)
-  end
-
-  def show
-    game_output_info
-    if @user.hand.scoring > @dealer.hand.scoring && !@user.hand.losing ||
-       !@user.hand.losing && @dealer.hand.losing
+  def show(user, dealer, bank)
+    game_output_info(user, dealer)
+    if user.hand.scoring > dealer.hand.scoring && !user.hand.losing ||
+       !user.hand.losing && dealer.hand.losing
       message('Вы выиграли!')
-      @bank.gain(@user)
-      main_info
-    elsif @user.hand.scoring == @dealer.hand.scoring && !@user.hand.losing
+      bank.gain(user)
+      main_info(user, dealer, bank)
+    elsif user.hand.scoring == dealer.hand.scoring && !user.hand.losing
       message('Ничья!')
-      game_return
-    elsif @user.hand.scoring < @dealer.hand.scoring && !@dealer.hand.losing ||
-          @user.hand.scoring > @dealer.hand.scoring && !@dealer.hand.losing
+      game_return(user, dealer, bank)
+    elsif user.hand.scoring < dealer.hand.scoring && !dealer.hand.losing ||
+          user.hand.scoring > dealer.hand.scoring && !dealer.hand.losing
       message('Вы проиграли!')
-      @bank.gain(@dealer)
-      main_info
+      bank.gain(dealer)
+      main_info(user, dealer, bank)
     else
       message('Перебор!')
-      game_return
+      game_return(user, dealer, bank)
     end
     separator
     show_menu
-  end
-
-
-  def dealer_move(num_of_cards)
-    @dealer.take_card(num_of_cards, @current_deck)
-  end
-
-  def user_move(num_of_cards)
-    @user.take_card(num_of_cards, @current_deck)
-  end
-
-  def again
-    if @user.cash >= 10 && @dealer.cash >= 10
-      @current_deck = Deck.new
-      @user.hand = Hand.new
-      user_move(2)
-      @user.bet(@bank, 10)
-      @dealer.hand = Hand.new
-      dealer_move(2)
-      @dealer.bet(@bank, 10)
-      main_info
-      game_card_hands
-      show_menu
-    else
-      message('Недостаточно средств для ставки. Игра окончена!')
-      @quit = 'break'
-    end
   end
 end
